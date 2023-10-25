93c1d0ede5f8b144e2a1c199cc48f6d3a1d45fcd
maxjeanty <maxjeanty@gmail.com>
2023-10-24 19:46:46 -0500

    CHANGED PLAY AGIN TO PLAY BUTTON TO MAKE POPUPWORK

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const wordEl = document.getElementById('word')
-const wrongLettersEl = document.getElementById('wrong-letters')
-const  playAgainBtn= document.getElementById('play-again')
-const popup  = document.getElementById('pop-container')
-const notification = document.getElementById('notification-container')
-const finalMessage = document.getElementById('final-message')
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('.figure-part')
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ["apple", "banana", "cherry", "dog", "elephant", "flower", "grape", "house", "ice cream", "jazz", "kite", "lemon", "mountain", "notebook", "orange"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)]
-console.log(selectedWord)
+const correctLetters = ['d','o', 'g']
+const wrongLetters = []
+
+
+// show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'BOOM! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+displayWord()
