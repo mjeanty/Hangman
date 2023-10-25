fa4c358e2a2d0fdc9dc6f4b326d1f4b3a4d263c7
maxjeanty <maxjeanty@gmail.com>
2023-10-24 21:59:37 -0500

    modified nesting and updated keycode

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const figureParts = document.querySelectorAll('.figure-part');
 const words = ["apple", "banana", "cherry", "dog", "elephant", "flower", "grape", "house", "ice cream", "jazz", "kite", "lemon", "mountain", "notebook", "orange"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)]
-const correctLetters = ['d','o', 'g']
+const correctLetters = []
 const wrongLetters = []
 
 
@@ -36,5 +36,54 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+// update the wrong letter
+function updateWrongLettersEl() {
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+  console.log(wrongLetters)
+}
 
+
+//show notification
+function showNotification(){
+  notification.classList.add('show')
+
+  setTimeout(()=>{
+    notification.classList.remove('show')
+
+  },2000)
+
+
+}
+
+
+
+// KEYBOARD LISTENER
+window.addEventListener('keydown', e => {
+  //pay atttention to nestings
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 displayWord()
